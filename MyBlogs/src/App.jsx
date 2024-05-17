f158177fdcf2fd28f7165521403fd3effe8edd92
VoidAshfak <1703103@student.ruet.ac.bd>
2024-05-17 06:44:16 +0600

    Store setup and basic components setup

--- MyBlogs/src/App.jsx
+++ MyBlogs/src/App.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom"
 
 function App() {
 
-    console.log(import.meta.env.VITE_APPWRITE_DATABASE_ID);
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch()
 
-    return (
-        <>
-            <h1>Blog app with appwrite</h1>
-        </>
-    )
+    useEffect(() => {
+        authService.getCurrentUser()
+            .then((userData) => {
+                if (userData) {
+                    dispatch(login({ userData }))
+                } else {
+                    dispatch(logout())
+                }
+            })
+            .finally(() => setLoading(false))
+    }, [])
+
+    return !loading ? (
+        <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+            <Header />
+            <main>
+                Todo: <Outlet />
+            </main>
+            {/* <Footer /> */}
+        </div>
+    ) : null
 }
 
 export default App
---
+++ MyBlogs/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({ width = "100px" }) {
+    return (
+        <img src="" alt="logo" width={width} />
+    )
+}
+
+export default Logo
